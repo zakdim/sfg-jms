94f71a95580ab5586c9a4f99bda8248531cfd55f
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2023-10-18 21:58:26 -0400

    s29-457 Sending JMS Messages

---
+++ src/main/java/guru/springframework/sfgjms/sender/HelloSender.java
@@ -0,0 +1,35 @@
+package guru.springframework.sfgjms.sender;
+
+import guru.springframework.sfgjms.config.JmsConfig;
+import guru.springframework.sfgjms.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+/**
+ * Create by dmitri on 2023-10-18.
+ */
+@RequiredArgsConstructor
+@Component
+public class HelloSender {
+
+    private final JmsTemplate jmsTemplate;
+
+    @Scheduled(fixedRate = 2000)
+    public void sendMessage() {
+
+        System.out.println("I'm sending a JMS message");
+        HelloWorldMessage message = HelloWorldMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("Hello JMS World!")
+                .build();
+
+        jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
+
+        System.out.println("JMS message sent!");
+    }
+}
